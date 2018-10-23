7b6242b42bd06300401e23a165a254a69c9f5c96
Jonathan Hernandez <jonathan1987@users.noreply.github.com>
2018-10-23 15:28:37 -0400

    Adding first custom gender feature classifier
    Adding first custom gender feature classifier; this one extracts the first 2 letters and last 2 letters.
    More to follow.

--- base-code-load-data.py
+++ base-code-load-data.py
@@ -14,3 +14,15 @@ names = ([(name, 'male') for name in names.words('male.txt')] +
 
 random.seed(4)
 random.shuffle(names)
+
+# Jonathan Hernandez
+def gender_feature_custom(name):
+    return {'suffix1': name[-1:],
+            'suffix2': name[-2:],
+            'prefix1': name[0],
+            'prefix2': name[0:2]}
+            
+featuresets = [(gender_feature_custom(n), g) for (n,g) in names]
+test_set, dev_test_set, train_set = featuresets[:500], featuresets[500:1000], featuresets[1000:]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print nltk.classify.accuracy(classifier, test_set)
